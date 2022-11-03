ca7dd5a0bbfbe2c2cef3e652cc829f09fe3d7e2d
Mike Delmonaco <mthundershock@gmail.com>
2022-11-02 22:36:49 -0400

    copy local expand to bs

---
+++ bs-manual-test.rkt
@@ -0,0 +1,229 @@
+#lang racket
+
+; tests classical.rkt
+
+(module+ test (require rackunit syntax/macro-testing))
+
+(require "./bs-manual.rkt")
+
+(module+ test
+  (test-case "simple class"
+    (define foo%
+      (class
+        (field x)
+        (define (add y) (+ x y))))
+    (define foo (new foo% 1))
+    (check-equal? (send foo add 2) 3))
+  (test-case "empty class"
+    (define foo% (class))
+    (new foo%)
+    (void))
+  (test-case "internal method call"
+    (define foo%
+      (class
+        (define (f x)
+          (send this g x))
+        (define (g x)
+          (add1 x))))
+    (define foo (new foo%))
+    (check-equal? (send foo f 1) 2))
+  (test-case "mutually recursive methods"
+    (define parity%
+      (class
+        (define (even? n)
+          (if (= n 0)
+              #t
+              (send this odd? (sub1 n))))
+        (define (odd? n)
+          (if (= n 0)
+              #f
+              (send this even? (sub1 n))))))
+    (define parity (new parity%))
+    (check-equal? (send parity even? 10) #t)
+    (check-equal? (send parity even? 11) #f))
+  (test-case "mutating a field"
+    (define counter%
+      (class
+        (field count)
+        (define (inc) (set! count (add1 count)))
+        (define (get) count)))
+    (define counter (new counter% 0))
+    (send counter inc)
+    (send counter inc)
+    (send counter inc)
+    (check-equal? (send counter get) 3))
+  (test-case "use a macro that expands to a definition"
+    (define-syntax-rule (m x) (define x (lambda () 2)))
+    (define foo%
+      (class
+        (m x)))
+    (define foo (new foo%))
+    (check-equal? (send foo x) 2))
+  (test-case "use a macro that expands to field"
+    (define-syntax-rule (m x) (field x))
+    (define foo%
+      (class
+        (m x)
+        (define (f) (add1 x))))
+    (define foo (new foo% 1))
+    (check-equal? (send foo f) 2))
+  (test-case "define and use a macro inside of a class"
+    (define foo%
+      (class
+        (define-syntax-rule (m x) (field x))
+        (define-syntax-rule (my-add1 x) (add1 x))
+        (m x)
+        (define (f) (my-add1 x))))
+    (define foo (new foo% 1))
+    (check-equal? (send foo f) 2))
+  (test-case "defines in a begin"
+    (define foo%
+      (class
+        (begin
+          (define-syntax-rule (m x) (field x))
+          (define-syntax-rule (my-add1 x) (add1 x))
+          (m x)
+          (define (f) (my-add1 x)))))
+    (define foo (new foo% 1))
+    (check-equal? (send foo f) 2))
+  (test-case "shadow method name"
+    (define foo%
+      (let* ([f 42]
+             [foo% (class (define (f) 2))])
+        foo%))
+    (define foo (new foo%))
+    (let ([f 42])
+      ; I am surprised that this passes lol
+      ; I guess in this position, it's not a reference so it's not bound
+      ; and when the method table is created, it is also not bound
+      (check-equal? (send foo f) 2)))
+  #;; TODO uncomment once you can apply methods normally
+  (test-case "macro expands to a reference to a locally defined value"
+    (define foo%
+      (class
+        (define-syntax-rule (call-g) (g))
+        (define (g) 2)
+        (define (f) (call-g))))
+    (define foo (new foo%))
+    (check-equal? (send foo f) 2))
+  (test-case "internal-definition-context-add-scopes regression test"
+    (define-syntax-rule (x) 'good)
+    (define-syntax-rule (m f) (define (f) (x)))
+    (define c%
+      (class
+        (define-syntax-rule (x) 'bad2)
+        (m f)))
+    (check-equal? (send (new c%) f) 'good))
+  (test-case "duplicate method name error"
+    (check-exn #rx"a method with same name has already been defined"
+               (lambda ()
+                 (convert-syntax-error
+                  (class
+                    (define (f) 2)
+                    (define (f) 2))))))
+  (test-case "duplicate method name error, macro introduced"
+    (check-exn #rx"a method with same name has already been defined"
+               (lambda ()
+                 (convert-syntax-error
+                  (let ()
+                    (define-syntax-rule (m) (define (f) 2))
+                    (class
+                      (m)
+                      (define (f) 2)))))))
+  (test-case "method used as procedure"
+    (define foo%
+      (class
+        (define (f) 2)
+        (define (g) (f))))
+    (define foo (new foo%))
+    (check-equal? (send foo g) 2))
+  (test-case "method used as procedure with args"
+    (define foo%
+      (class
+        (define (f x y) (+ x y))
+        (define (g) (f 1 2))))
+    (define foo (new foo%))
+    (check-equal? (send foo g) 3))
+  (test-case "method used as reference"
+    (define foo%
+      (class
+        (define (f) 2)
+        (define (g) f)))
+    (define foo (new foo%))
+    (check-pred procedure? (send foo g)))
+  (test-case "method used in apply"
+    (define foo%
+      (class
+        (define (f x y) (+ x y))
+        (define (g) (apply f (list 1 2)))))
+    (define foo (new foo%))
+    (check-equal? (send foo g) 3))
+  (test-case "class-level expr"
+    (define v 'bad)
+    (define foo%
+      (class
+        (set! v 'good)))
+    (new foo%)
+    (check-equal? v 'good))
+  (test-case "multiple class-level exprs"
+    (define v 0)
+    (define foo%
+      (class
+        (set! v (add1 v))
+        (set! v (add1 v))))
+    (new foo%)
+    (check-equal? v 2))
+  (test-case "multiple class-level exprs"
+    (define v 0)
+    (define foo%
+      (class
+        (set! v (add1 v))
+        (set! v (add1 v))))
+    (new foo%)
+    (check-equal? v 2))
+  (test-case "class-level field usage"
+    (define foo%
+      (class
+        (field v)
+        (set! v 'good)
+        (define (get-v) v)))
+    (define foo (new foo% #f))
+    (check-equal? (send foo get-v) 'good))
+  (test-case "class-level method usage"
+    (define v 'bad)
+    (define foo%
+      (class
+        (define (m) (set! v 'good))
+        (m)))
+    (new foo%)
+    (check-equal? v 'good))
+  (test-case "method shadows macro"
+    (define v #f)
+    (define-syntax-rule (m e ...) (set! v 'bad))
+    (define foo%
+      (class
+        (define (m) (set! v 'good))
+        (m)))
+    (new foo%)
+    (check-equal? v 'good))
+  (test-case "method shadows macro after use"
+    ; I was surprised, but racket class works this way too!
+    ; The expansion depends on the order
+    ; Makes sense since the expr is actually expanded before the method is bound
+    (define v #f)
+    (define-syntax-rule (m e ...) (set! v 'good))
+    (define foo%
+      (class
+        (m)
+        (define (m) (set! v 'bad))))
+    (new foo%)
+    (check-equal? v 'good))
+  (test-case "local macro use before definition"
+    ; this error is weird, but it's the same behavior as module-level racket
+    (check-exn
+     #rx"use does not match pattern"
+     (lambda ()
+       (convert-compile-time-error
+        (class
+          (m)
+          (define-syntax-rule (m) (set! x 1))))))))
---
+++ bs-manual.rkt
@@ -0,0 +1,335 @@
+#lang racket
+
+; This file is a "local-expand loop" style implementation of a small class system.
+; Similar to the implementation of the 'block' macro.
+; This improves over the simple implementation by allowing class-level macro uses and definitions.
+
+(module+ test (require rackunit))
+(provide (all-defined-out))
+
+(require racket/stxparam syntax/transformer (for-syntax syntax/context syntax/intdef syntax/parse syntax/transformer))
+
+(struct class-info [name->method-index method-table constructor])
+; A ClassInfo is a (class-info (identifier -> natural) (any ... -> Object)) where
+; name->method-index maps a method name to its vector index in the method-table
+; method-table is a vector of methods
+; Represents a class itself
+
+(struct object [fields class])
+; An Object is a (object (vector any) (vector Method) Class) where
+; fields is a vector of field-values
+; class is the class of which this object is an instance
+; Represents an object, which is an instance of a class
+
+; A Method is a (any any ... -> any)
+; Where the first argument is "this"
+; Represents a method on a class
+
+; define literals that error out when used outside of a class
+(define-syntax define-class-literals
+  (syntax-parser
+    [(_ lit:id ...)
+     #'(begin (define-syntax lit (make-literal-transformer 'lit))
+              ...)]))
+
+(begin-for-syntax
+  #;(symbol? -> transformer?)
+  ; creates a transformer that errors out when used outside of a class
+  (define (make-literal-transformer id-sym)
+    (syntax-parser
+      [_ (raise-syntax-error id-sym "used outside of a class" this-syntax)])))
+
+; define syntax parameters that error out when used outside of a class
+(define-syntax define-class-syntax-parameters
+  (syntax-parser
+    [(_ name:id ...)
+     #'(begin (define-syntax-parameter name (make-literal-transformer 'name))
+              ...)]))
+
+(define-class-literals field)
+(define-class-syntax-parameters this this%)
+
+#|
+before matching, local expand with stop-words field and define-values, then analyze
+
+also support define-syntaxes in the class (another stop-name)
+
+consult block macro that michael sent you!!!
+https://github.com/racket/racket/blob/a17621bec9216edd02b44cc75a2a3ad982f030b7/racket/collects/racket/block.rkt
+
+scoping rules too
+
+as you expand to definitions, you need to make bindings
+
+syntax-local-bind-syntaxes
+
+internal-definition-context-add-scopes for inside outside edge (block doens't do it)
+
+
+
+next steps:
+- [x] symbolic equality for method names
+  - uniqueness check
+  - test macro-introduced binding and surface binding are considered equal and in conflict
+- [x] bind method names and support references to them
+- [x] support top-level expressions
+- bindingspec-style local expansion:
+  - instead of outputting define-syntax, local-expand value rhs with the def ctx that has the macros
+  - when you do that,
+    - something like #%host-expr, compile-binder!, compile-reference
+    - suspension and resumption
+    - bind surface names to transformers that ref a free id table that will end up mapping them to compiled names in the output code
+    - initially, this lookup would fail. But running something like compile-binder! on a binder would make an entry in the table.
+    - references will be the surface identifiers, so they'll expand via the transformer. No real need for compile-reference I think, since the transformer will take care of it.
+    - scope stuff for compile-binder!: you'll find out! something with syntax-local-get-shadower on the reference.
+    - for #%host-expr, wrap expr positions in #%host-expr and add a stx prop containing the def ctx.
+      #%host-expr will get that prop and local-expand its argument under that def ctx
+    - eventually, we'll replace local-expand with syntax-local-expand-expressoin to avoid re-expansion after outputting local-expanded code
+
+The current bindingspec-style method has quadratic re-expansions. If you have nested classes (inside of parents' expression positions),
+the first class' syntax local-expands and outputs syntax that needs to be re-expanded. Then, its parent local-expands, which re-expands the first class.
+Then, its parent local expands, which re-expands both classes. And so-on. You get triangular (quadratic) re-expansions.
+
+Eager expansion (expand rhs before compilation) wouldn't work.
+- The syntax definitions get evaluated twice, which is inefficient and is really bad if they are effectful
+- They are evaluated once during syntax-local-bind-syntaxes, and again when the emitted letrec-syntaxes expands
+
+Currently, you have to choose between:
+- macro transformers being evaluated twice (bind macros in pass 1 for definition-emitting macros, and re-emit them in the output syntax so
+"phase 2" (expansion of emitted syntax) has access to them to expand method rhs and top-level exprs)
+- quadratic re-expansion with bindingspec-style suspensions
+
+the syntax-local-expand-expression change will allow us to create opaque suspensions with access to transformers that will only get expanded once, never local-expanded
+We will get the best of both worlds.
+Macro definitions won't have to be emitted, so they'll only be evaluated once when the suspension is created.
+And we won't have to local-expand suspensions, they'll just expand with the transformers in context.
+|#
+
+; initially copied from https://github.com/racket/racket/blob/a17621bec9216edd02b44cc75a2a3ad982f030b7/racket/collects/racket/block.rkt
+(define-syntax class
+  (make-expression-transformer
+   (lambda (stx)
+     (let ([def-ctx (syntax-local-make-definition-context)])
+       ; If this was going to get more complicated, I'd do more pre-processing into appropriate structured data
+       (let-values ([(stx-defns defns fields exprs) (group-class-decls (local-expand-class-body stx def-ctx))])
+         (compile-class-body defns stx-defns fields exprs def-ctx))))))
+
+(begin-for-syntax
+  ; copied from https://github.com/racket/racket/blob/a17621bec9216edd02b44cc75a2a3ad982f030b7/racket/collects/racket/private/intdef-util.rkt
+  (define (add-decl-props def-ctx decls stx)
+    (internal-definition-context-track
+     def-ctx
+     (for/fold ([stx stx]) ([decl (in-list decls)])
+       (define (copy-prop src dest stx)
+         (syntax-property
+          stx
+          dest
+          (cons (syntax-property decl src)
+                (syntax-property stx dest))))
+       (copy-prop
+        'origin 'disappeared-use
+        (copy-prop
+         'disappeared-use 'disappeared-use
+         (copy-prop
+          'disappeared-binding 'disappeared-binding
+          stx))))))
+  #;(syntax? definition-context? -> (listof syntax?))
+  ; expand the body of the class expression using the given definition context
+  ; returns a list of (partially) expanded class-level forms.
+  ; expands to just field declarations and definitions (of values and syntaxes).
+  ; does not expand rhs of define-values, only define-syntaxes.
+  (define (local-expand-class-body stx def-ctx)
+    (let*
+        ([ctx (generate-expand-context #t)]
+         [stoplist (list #'begin #'define-syntaxes #'define-values #'field #'lambda)]
+         [init-exprs (let ([v (syntax->list stx)])
+                       (unless v (raise-syntax-error #f "bad syntax" stx))
+                       (map (λ (expr) (internal-definition-context-add-scopes def-ctx expr))
+                            (cdr v)))])
+      (let loop ([todo init-exprs] [r '()])
+        (if (null? todo)
+            (reverse r)
+            (let ([expr (local-expand (car todo) ctx stoplist def-ctx)]
+                  [todo (cdr todo)])
+              (syntax-parse expr
+                #:literals (begin define-syntaxes define-values field)
+                [(begin . rest)
+                 ; splice the begin
+                 (loop (append (syntax->list #'rest) todo) r)]
+                [(define-syntaxes (id:id ...) rhs)
+                 ; bind ids to transformers in the def-ctx
+                 (with-syntax ([rhs (local-transformer-expand #'rhs 'expression null)])
+                   (with-syntax ([(id ...) (syntax-local-bind-syntaxes (syntax->list #'(id ...))
+                                                                       #'rhs
+                                                                       def-ctx)])
+                     (loop todo (cons (datum->syntax
+                                       expr
+                                       (list #'define-syntaxes #'(id ...) #'rhs)
+                                       expr
+                                       expr)
+                                      r))))]
+                [(define-values (id:id ...) rhs)
+                 (with-syntax ([(id ...) (syntax-local-bind-syntaxes (syntax->list #'(id ...)) #f def-ctx)])
+                   (loop todo (cons (datum->syntax
+                                     expr
+                                     (list #'define-values #'(id ...) #'rhs)
+                                     expr
+                                     expr)
+                                    r)))]
+                [(field id:id ...)
+                 (with-syntax ([(id ...) (syntax-local-bind-syntaxes (syntax->list #'(id ...)) #f def-ctx)])
+                   (loop todo (cons (datum->syntax
+                                     expr
+                                     ; block does this slightly differently, be careful
+                                     #'(field id ...)
+                                     expr
+                                     expr)
+                                    r)))]
+                ; This is a plain top-level expression to be evaluated in the constructor
+                ; Leave as-is
+                [_ (loop todo (cons this-syntax r))]))))))
+
+  #;((listof syntax?) -> (values (listof syntax?) (listof syntax?) (listof syntax?) (listof syntax?)))
+  ; accepts a list of partially expanded class-level definitions and returns them grouped into
+  ; syntax definitions, value definitions, field declarations, and top-level exprs
+  (define (group-class-decls exprs)
+    (let loop ([exprs exprs]
+               ; list of (define-syntaxes ...) exprs
+               [prev-stx-defns null]
+               ; list of (define-values ...) exprs
+               [prev-defns null]
+               ; list of (field id ...) exprs
+               [prev-fields null]
+               [prev-exprs null])
+      (syntax-parse exprs
+        [(expr . rest-exprs)
+         (syntax-parse #'expr
+           #:literals (define-syntaxes define-values field)
+           [(define-syntaxes . _)
+            (loop #'rest-exprs
+                  (cons #'expr prev-stx-defns)
+                  prev-defns
+                  prev-fields
+                  prev-exprs)]
+           [(define-values . _)
+            (loop #'rest-exprs
+                  prev-stx-defns
+                  (cons #'expr prev-defns)
+                  prev-fields
+                  prev-exprs)]
+           [(field . _)
+            (loop #'rest-exprs
+                  prev-stx-defns
+                  prev-defns
+                  (cons #'expr prev-fields)
+                  prev-exprs)]
+           [_
+            (loop #'rest-exprs
+                  prev-stx-defns
+                  prev-defns
+                  prev-fields
+                  (cons #'expr prev-exprs))])]
+        [() (values (reverse prev-stx-defns)
+                    (reverse prev-defns)
+                    (reverse prev-fields)
+                    (reverse prev-exprs))])))
+
+  #;((listof syntax?) (listof syntax?) (listof syntax?) (listof syntax?) definition-context? -> syntax?)
+  ; compile the partially expanded class-level definitions into pure racket code.
+  ; This is the actual class logic.
+  (define (compile-class-body defns stx-defns fields exprs def-ctx)
+    (add-decl-props
+     def-ctx
+     (append stx-defns defns)
+     ; TODO better error messages
+     (syntax-parse (list stx-defns defns fields exprs)
+       #:literals (define-values field)
+       [(((define-syntaxes (stx-name:id ...) stx-rhs:expr) ...)
+         ; I know ~datum for lambda is bad, but I don't know how to do this correctly
+         ; There are at least two distinct (by free-identifier=?) "lambda"s that could end up here
+         ((define-values (method-name:id) ((~datum lambda) (method-arg:id ...) method-body:expr ...)) ...)
+         ; only 1 field definition allowed
+         ((~optional (field field-name:id ...) #:defaults ([(field-name 1) null])))
+         (expr ...))
+        (check-duplicate-method-names (attribute method-name))
+        (define num-fields (length (attribute field-name)))
+        (define/syntax-parse (field-index ...) (build-list num-fields (lambda (n) #`#,n)))
+        #'(let ()
+            (letrec-syntaxes ([(stx-name ...) stx-rhs]
+                              ...
+                              [(method-name) (make-variable-like-transformer #'(lambda args (send this method-name . args)))]
+                              ...)
+              (letrec ([method-table
+                        (vector (lambda (this-arg method-arg ...)
+                                  ; to support class-level expressions that may call methods and fields,
+                                  ; this will have to be done around class-level expressions too
+                                  (let ([fields (object-fields this-arg)])
+                                    (let-syntax ([field-name (make-vector-ref-transformer #'fields #'field-index)]
+                                                 ...)
+                                      (syntax-parameterize ([this (make-variable-like-transformer #'this-arg)])
+                                        method-body
+                                        ...))))
+                                ...)]
+                       [constructor
+                        (lambda (field-name ...)
+                          (let ([this-val (object (vector field-name ...) cls)])
+                            (let ([fields (object-fields this-val)])
+                              (let-syntax ([field-name (make-vector-ref-transformer #'fields #'field-index)]
+                                           ...)
+                                (syntax-parameterize ([this (make-variable-like-transformer #'this-val)])
+                                  ; I'm just putting this here to ensure that the body is non-empty
+                                  ; That's ok, right?
+                                  (void)
+                                  expr
+                                  ...)))
+                            this-val))]
+                       [method-name->index
+                        (make-name->index (list #'method-name ...))]
+                       [cls
+                        (class-info method-name->index method-table constructor)])
+                cls)))])))
+  #;((listof identifier?) -> void?)
+  ; If there are (symbolically) duplicate method names, error
+  (define (check-duplicate-method-names names)
+    (let loop ([ids names] [seen-symbols '()])
+      (cond
+        [(null? ids) (void)]
+        [(member (syntax->datum (car ids)) seen-symbols)
+         (raise-syntax-error #f "a method with same name has already been defined" (car ids))]
+        [else
+         (loop (cdr ids) (cons (syntax->datum (car ids)) seen-symbols))]))))
+
+#;((listof identifier?) -> (identifier? -> natural?))
+; Create a function that maps method names to their method table indices
+(define (make-name->index names)
+  (let ([table (make-hasheq (map (lambda (id index) (cons (syntax->datum id) index))
+                                 names
+                                 (build-list (length names) identity)))])
+    (lambda (name)
+      (hash-ref table (syntax->datum name) (lambda () (error 'send "no such method ~a" name))))))
+
+(begin-for-syntax
+  ; Creates a set!-transformer that accesses and mutates an elment of a vector
+  (define (make-vector-ref-transformer vector-stx index-stx)
+    (make-variable-like-transformer
+     #`(vector-ref #,vector-stx #,index-stx)
+     #`(lambda (v) (vector-set! #,vector-stx #,index-stx v)))))
+
+(define (new cls . fields)
+  (apply (class-info-constructor cls) fields))
+
+(define-syntax send
+  (syntax-parser
+    [(_ obj:expr method-name:id arg:expr ...)
+     #'(send-rt obj #'method-name (list arg ...))]
+    [(_ obj:expr method-name:id . args)
+     #'(send-rt obj #'method-name args)]))
+
+(define (send-rt obj method-name-stx args)
+  (let* ([cls (object-class obj)]
+         [index ((class-info-name->method-index cls) method-name-stx)]
+         [method-table (class-info-method-table cls)]
+         [method (vector-ref method-table index)])
+    (apply method obj args)))
+
+(module+ test)
